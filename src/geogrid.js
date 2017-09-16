4f65a63e6b2b277c8cb6601ea9d934edfbe39d89
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2017-09-16 22:18:32 +0200

    minor bug

--- src/geogrid.js
+++ src/geogrid.js
@@ -254,7 +254,7 @@ L.ISEA3HLayer = L.Layer.extend({
   },
   _uuidv4: function() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+      let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
       return v.toString(16)
     })
   },
