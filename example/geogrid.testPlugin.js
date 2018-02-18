9e84b5d8d67de64a809264bfea438d125cb09817
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-02-18 13:21:27 +0100

    example with plugin introduced

---
+++ example/geogrid.testPlugin.js
@@ -0,0 +1,24 @@
+"use strict"
+
+if (typeof L === 'undefined') throw '[geogrid.js] Leaflet and geogrid.js need to be loaded first'
+
+L.TestPlugin = class TestPlugin extends L.ISEA3HLayerPlugin {
+  onHover(e) {
+    this.setCellColor(e.cell, 'blue')
+    this.render()
+    // console.debug('hover', e.cell.id)
+  }
+  onUnhover(e) {
+    this.setCellColor(e.cell, null)
+    this.render()
+    // console.debug('unhover', e.cell.id)
+  }
+  onClick(e) {
+    this.neighbors(e.cell, ns => console.debug(ns))
+    // this.setParameter('dateFrom', '2017-01-01')
+    // this.downloadData()
+    // console.debug('click', e.cell.id, e.cell.value)
+  }
+}
+
+L.testPlugin = () => new L.TestPlugin()
