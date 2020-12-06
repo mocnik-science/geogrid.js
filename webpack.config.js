a2513beca066e41104e80e83d70e012289656a51
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2020-12-07 00:39:33 +0100

    dependencies updated

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
       test: /\.js$/,
       loader: 'babel-loader',
       query: {
-        presets: ['es2015'],
+        presets: ['env'],
       },
     }, {
       test: /\.scss$/,
