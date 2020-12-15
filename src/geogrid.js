8a992de6450b221e8edd71071fc624d357ef46b5
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2020-12-15 22:38:43 +0100

    minor bug

--- src/geogrid.js
+++ src/geogrid.js
@@ -139,13 +139,13 @@ if (leafletLoaded && d3Loaded) L.ISEA3HLayer = L.Layer.extend({
     }
 
     // event for plugin
-    const eventForPlugin = cell => ({
+    const eventForPlugin = cell => cell === null || cell === undefined ? null : {
       lat: cell.lat,
       lon: cell.lon,
       cell: cell,
       data: this._data.dataForId(cell.id),
       ...(this.options.dataMap !== null ? {dataNotMapped: this._data.dataForIdNotMapped(cell.id)} : {}),
-    })
+    }
   },
   onAdd: function(map) {
     this._map = map
